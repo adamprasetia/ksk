17cff8193a04dffad072abd1b124aa65cf79c80e
Adam Prasetia <adam.prasetia@gmail.com>
2015-11-22 10:49:52 +0700

    add reminder oli mesin

---
+++ application/helpers/reminder_helper.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+function olie_mesin_status($lama){
+	$r = explode(" ",$lama);
+	if($r[0] >= 5 && $r[1]== 'bulan'){
+		return '<label class="label label-danger">Danger</label>';
+	}elseif($r[0] >= 1 && $r[1]== 'bulan'){
+		return '<label class="label label-warning">Warning</label>';
+	}else{
+		return '<label class="label label-success">Sehat</label>';
+	}
+}
---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<div class="panel panel-default">
+	<div class="panel-heading">
+		Reminder Ganti Oli Mesin	
+	</div>	
+	<div class="panel-body">
+		<div class="table-responsive">
+			<?=$reminder_kendaraan?>
+		</div>	
+	</div>	
+</div>	
