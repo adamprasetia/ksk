50233f25d4a85b7bc43e19d29450be03748a1149
Adam Prasetia <adam.prasetia@gmail.com>
2015-11-28 03:18:27 +0700

    add google analytic

--- application/views/login.php
+++ application/views/login.php
@@ -11,6 +11,16 @@
 	<link href="<?=base_url('assets/css/signin.css')?>" type="text/css" rel="stylesheet"/>
 	<link href="<?=base_url('assets/css/sticky-footer.css')?>" type="text/css" rel="stylesheet"/>
 	<link href="<?=base_url('assets/css/style.css')?>" type="text/css" rel="stylesheet"/>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-70699667-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>	
 </head>
 <body>
 	<nav class="navbar navbar-default navbar-fixed-top">
